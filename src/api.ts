0c05d18c349903337553332e2d824bcbaa8ee5c2
kevin-lupera <kevinlupera@gmail.com>
2023-10-15 18:39:08 -0500

    Auth

--- src/api.ts
+++ src/api.ts
@@ -1,11 +1,10 @@
 import { Context, Hono } from "hono";
-// import * as model from "./model";
-import { Bindings } from "./bindings";
 import { cors } from "hono/cors";
+import { Bindings } from "./bindings";
 
 const ROWS_BY_PAGE: number = 10;
-
 const api = new Hono<{ Bindings: Bindings }>();
+
 api.use("/events/*", cors());
 api.use("/version/*", cors());
 api.use("/categories/*", cors());
@@ -20,6 +19,7 @@ api.get("/version", async (c) => {
     let results = await c.env.DB.prepare("SELECT * FROM versions").first();
     return c.json(results);
   } catch (e) {
+    console.error(e)
     return c.json({ err: e }, 500);
   }
 });
@@ -144,7 +144,7 @@ async function getTotalEvents(
     ).first("count");
     return parseInt(count);
   } catch (e) {
-    console.error(e)
+    console.error(e);
     return 0;
   }
 }
@@ -165,8 +165,7 @@ api.get("/events", async (c) => {
 
     const offset = ROWS_BY_PAGE * (page - 1);
 
-    const filterGeneralCountry =
-      `and (country like '%${country}%' or country like '%general%')`;
+    const filterGeneralCountry = `and (country like '%${country}%' or country like '%general%')`;
     const total: number = await getTotalEvents(
       c,
       idCategory,
@@ -201,163 +200,6 @@ api.get("/events/:id", async (c) => {
     return c.json(results[0]);
   } catch (e) {
     console.error(e);
-    return c.json({ err: e }, 500);
-  }
-});
-// api.get("/events/:id", async (c) => {
-//   try {
-//     const id = c.req.param("id");
-//     let { results } = await c.env.DB.prepare(
-//       "SELECT * FROM categories WHERE id = ? ORDER BY id DESC"
-//     )
-//       .bind(id)
-//       .all();
-//     if (results.length == 0) {
-//       return c.json({ err: "NOT FOUND" }, 404);
-//     }
-//     return c.json(results[0]);
-//   } catch (e) {
-//     console.error(e);
-//     return c.json({ err: e }, 500);
-//   }
-// });
-
-// api.post("/events", async (c) => {
-//   try {
-//     const { name }: any = await c.req.json();
-//     let { results } = await c.env.DB.prepare(
-//       "INSERT INTO categories (name) VALUES (?) RETURNING id"
-//     )
-//       .bind(name)
-//       .all();
-//     let result = await c.env.DB.prepare(
-//       "SELECT * FROM categories WHERE id = ? ORDER BY id DESC"
-//     )
-//       .bind(results[0].id)
-//       .first();
-//     return c.json(result);
-//   } catch (e) {
-//     console.error(e);
-//     return c.json({ err: e }, 500);
-//   }
-// });
-
-// api.put("/events/:id", async (c) => {
-//   try {
-//     const id = c.req.param("id");
-//     const { name }: any = await c.req.json();
-//     if (id != null) {
-//       let { results } = await c.env.DB.prepare(
-//         "SELECT id FROM categories WHERE id = ? ORDER BY id DESC"
-//       )
-//         .bind(id)
-//         .all();
-//       if (results.length == 0) {
-//         return c.json({ err: "NOT FOUND" }, 404);
-//       }
-//     }
-
-//     let { results } = await c.env.DB.prepare(
-//       "UPDATE categories SET name = ? WHERE id = ? RETURNING *"
-//     )
-//       .bind(name, id)
-//       .all();
-//     return c.json(results[0]);
-//   } catch (e) {
-//     console.error(e);
-//     return c.json({ err: e }, 500);
-//   }
-// });
-
-// api.delete("/events/:id", async (c) => {
-//   try {
-//     const id = c.req.param("id");
-//     if (id != null) {
-//       let { results } = await c.env.DB.prepare(
-//         "SELECT id FROM categories WHERE id = ? ORDER BY id DESC"
-//       )
-//         .bind(id)
-//         .all();
-//       if (results.length == 0) {
-//         return c.json({ err: "NOT FOUND" }, 404);
-//       }
-//     }
-//     let { results } = await c.env.DB.prepare(
-//       "DELETE FROM categories WHERE id = ? RETURNING 1"
-//     )
-//       .bind(id)
-//       .all();
-//     const success = results ? true : false;
-//     return c.json({ code: success });
-//   } catch (e) {
-//     console.error(e);
-//     return c.json({ err: e }, 500);
-//   }
-// });
-
-// api.get("/posts", async (c) => {
-//   const posts = await model.getPosts(c.env.BLOG_EXAMPLE);
-//   return c.json({ posts: posts, ok: true });
-// });
-
-// api.post("/posts", async (c) => {
-//   const param = await c.req.json();
-//   const newPost = await model.createPost(
-//     c.env.BLOG_EXAMPLE,
-//     param as model.Param
-//   );
-//   if (!newPost) {
-//     return c.json({ error: "Can not create new post", ok: false }, 422);
-//   }
-//   return c.json({ post: newPost, ok: true }, 201);
-// });
-
-// api.get("/posts/:id", async (c) => {
-//   const id = c.req.param("id");
-//   const post = await model.getPost(c.env.BLOG_EXAMPLE, id);
-//   if (!post) {
-//     return c.json({ error: "Not Found", ok: false }, 404);
-//   }
-//   return c.json({ post: post, ok: true });
-// });
-
-// api.put("/posts/:id", async (c) => {
-//   const id = c.req.param("id");
-//   const post = await model.getPost(c.env.BLOG_EXAMPLE, id);
-//   if (!post) {
-//     // 204 No Content
-//     return new Response(null, { status: 204 });
-//   }
-//   const param = await c.req.json();
-//   const success = await model.updatePost(
-//     c.env.BLOG_EXAMPLE,
-//     id,
-//     param as model.Param
-//   );
-//   return c.json({ ok: success });
-// });
-
-// api.delete("/posts/:id", async (c) => {
-//   const id = c.req.param("id");
-//   const post = await model.getPost(c.env.BLOG_EXAMPLE, id);
-//   if (!post) {
-//     // 204 No Content
-//     return new Response(null, { status: 204 });
-//   }
-//   const success = await model.deletePost(c.env.BLOG_EXAMPLE, id);
-//   return c.json({ ok: success });
-// });
-
-api.get("/event/:id", async (c) => {
-  const userId = c.req.param("id");
-  try {
-    let { results } = await c.env.DB.prepare(
-      "SELECT * FROM users WHERE user_id = ?"
-    )
-      .bind(userId)
-      .all();
-    return c.json(results);
-  } catch (e) {
     return c.json({ err: e }, 500);
   }
 });
--- src/index.ts
+++ src/index.ts
@@ -1,31 +1,50 @@
 import { Hono } from "hono";
-import { cache } from "hono/cache";
 import { cors } from "hono/cors";
 import { prettyJSON } from "hono/pretty-json";
+import { basicAuth } from "hono/basic-auth";
+// import { cache } from "hono/cache";
+// import { secureHeaders } from "hono/secure-headers";
 import { api } from "./api";
 import { Bindings } from "./bindings";
 
 const app = new Hono();
+const token = "rwuy6434tgdgjhtiojiosi838tjue3";
+
 // Prettier
 app.use("*", prettyJSON({ space: 4 }));
 // Cors
 app.use("/api/*", cors());
+
+// app.use(
+//   "*",
+//   secureHeaders({
+//     xFrameOptions: false,
+//     xXssProtection: false,
+//   })
+// );
+
 // Cache
-app.get(
-  "*",
-  cache({
-    cacheName: "my-app",
-    cacheControl: "max-age=3600",
-  })
-);
+// app.get(
+//   "*",
+//   cache({
+//     cacheName: "my-app",
+//     cacheControl: "max-age=3600",
+//   })
+// );
 
 app.get("/", (c) => c.text("Pretty API"));
 // Error 404
-app.notFound((c) => c.json({ message: "Not Found", ok: false }, 404));
+app.notFound((c) => c.json({ message: "Not Found", code: 404 }, 404));
 
 const middleware = new Hono<{ Bindings: Bindings }>();
 middleware.use("*", prettyJSON());
-
+app.use(
+  "/api/*",
+  basicAuth({
+    username: "tvply",
+    password: token,
+  })
+);
 // API
 app.route("/api", middleware);
 app.route("/api", api);
